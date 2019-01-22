e9fdfd09b49e47414204f70cc7bcbbcd0d70fe20
Austin Chong <Austin@Austins-MBP.fios-router.home>
2019-01-22 00:08:28 -0500

    fix typo

--- src/components/Registry/Registry.js
+++ src/components/Registry/Registry.js
@@ -8,7 +8,7 @@ export default class Registry extends Component {
       <section id='registry' className='text-center'>
         <h2>Our Registry</h2>
         <p style={{'margin-bottom': '0'}}>Amazon</p>
-        <a href='https://www.amazon.com/wedding/austin-chong-michelle-noh-leesburg-april-2019/registry/7GPMPKRZU8O0' target='_blank' rel="noopener noreferrer" style={{'font-size': '20px'}}>View our Regsitry</a>
+        <a href='https://www.amazon.com/wedding/austin-chong-michelle-noh-leesburg-april-2019/registry/7GPMPKRZU8O0' target='_blank' rel="noopener noreferrer" style={{'font-size': '20px'}}>View our Registry</a>
       </section>
     );
   }
